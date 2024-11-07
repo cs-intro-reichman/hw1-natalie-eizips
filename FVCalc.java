8539d94f477fb0427ec89e50d47efe535c5d8d01
natalie-eizips <104609729+natalie-eizips@users.noreply.github.com>
2024-11-07 15:35:30 +0200

    Update

--- FVCalc.java
+++ FVCalc.java
@@ -3,7 +3,7 @@ public class FVCalc {
 	public static void main(String[] args){
 
         int current_value = Integer.parseInt(args[0]);
-        int percentage = Integer.parseInt(args[1]);
+        double percentage = Double.parseDouble(args[1]);
         int years = Integer.parseInt(args[2]);
 
         double rate = ((double) percentage) / 100;
@@ -11,6 +11,6 @@ public class FVCalc {
         double future_value = current_value * Math.pow(1 + rate, years);
         int future_value1 = (int) future_value;
 
-        System.out.println("After " + years + " years, $" + current_value + " saved at " + percentage + "% will yield at $" + future_value1 + ".");
+        System.out.println("After " + years + " years, $" + current_value + " saved at " + percentage + "% will yield at $" + future_value1);
     }
 }
