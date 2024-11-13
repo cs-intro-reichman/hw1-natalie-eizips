be5cb7b88e3084f8049291fcab2939b45be6c3c0
natalie-eizips <104609729+natalie-eizips@users.noreply.github.com>
2024-11-13 18:06:26 +0200

    Update

--- Bill3.java
+++ Bill3.java
@@ -11,6 +11,6 @@ public class Bill3 {
 
 		double payment = Math.round((bill / 3) + 0.49);
 
-		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + payment + " Shekels each");	
+		System.out.println("Dear " + name3 + ", " + name2 + ", and" + name1 + ": pay " + payment + " Shekels each");	
 	}
 }
